aadd56ff3ef85c6936e0e86eb37f09b4b08859e3
Robert Canas <robert_canas@hotmail.com>
2017-02-14 01:40:45 +0000

    Addition of Step 5,request file made

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to GitHub Avatar Downloader!');
 
@@ -7,8 +8,22 @@ var GITHUB_TOKEN = "321b033b375060fd93ef57506f6e0b11eb996425";
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-console.log(requestURL);
+  var requestURL = {
+    url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+
+  request.get(requestURL)
+    .on('error', function(err) {
+      throw err;
+    })
+    .on('response', function(response) {
+
+      console.log('Response Status Code: ', response.statusCode);
+    })
+    .pipe(fs.createWriteStream('./requestFile.txt'));
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
