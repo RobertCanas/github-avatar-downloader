171dae09321534beb6e56c36afe131fe8c84aa98
Robert Canas <robert_canas@hotmail.com>
2017-02-14 03:01:25 +0000

    Commit step 6

--- download_avatars.js
+++ download_avatars.js
@@ -5,9 +5,8 @@ console.log('Welcome to GitHub Avatar Downloader!');
 
 var GITHUB_USER = "RobertCanas";
 var GITHUB_TOKEN = "321b033b375060fd93ef57506f6e0b11eb996425";
-
-
 function getRepoContributors(repoOwner, repoName, cb) {
+
   var requestURL = {
     url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
@@ -15,18 +14,39 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
-  request.get(requestURL)
-    .on('error', function(err) {
-      throw err;
-    })
-    .on('response', function(response) {
 
-      console.log('Response Status Code: ', response.statusCode);
-    })
-    .pipe(fs.createWriteStream('./requestFile.txt'));
+  request.get(requestURL, function(error, response){
+    //console.log(JSON.parse(response.body));
+    extract_avatarUrl(JSON.parse(response.body));
+  })
+
+  function extract_avatarUrl(response){
+    response.map(function(cv, index){
+      console.log(cv.avatar_url);
+    });
+  }
+
+  function downloadImageByURL(url, filePath) {
+  // ...
+  }
+
+
+  //.pipe(fs.createWriteStream('./requestFile.txt'));
+
+    // .on('error', function(err) {
+    //   throw err;
+    // })
+    // .on('response', function(response) {
+    //   console.log(response.body);
+    //   console.log('Response Status Code: ', response.statusCode);
+    //  //console.log(response);
+    // })
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
+  // result.map(function(cv, index){
+  //   console.log(cv);
+  // });
 });
