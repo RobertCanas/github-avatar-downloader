6f73fedecb00f77569e2e73d3e301ba23780d4f2
Robert Canas <robert_canas@hotmail.com>
2017-02-14 04:35:21 +0000

    Step 8 Implementation

--- download_avatars.js
+++ download_avatars.js
@@ -5,6 +5,17 @@ console.log('Welcome to GitHub Avatar Downloader!');
 
 var GITHUB_USER = "RobertCanas";
 var GITHUB_TOKEN = "321b033b375060fd93ef57506f6e0b11eb996425";
+
+
+function downloadImageByURL(url, filePath) {
+  // ...
+  request.get(url)
+    .on('error', function(err) {
+      throw err;
+    })
+    .pipe(fs.createWriteStream(filePath));
+}
+
 function getRepoContributors(repoOwner, repoName, cb) {
 
   var requestURL = {
@@ -14,36 +25,23 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
+  // function extract_avatarUrl(response){
+  //   response.map(function(cv, index){
+  //     //console.log(cv.avatar_url);
+  //   });
+  // }
 
-  request.get(requestURL, function(error, response){
-    extract_avatarUrl(JSON.parse(response.body));
-  })
-
-  function extract_avatarUrl(response){
-    response.map(function(cv, index){
-      console.log(cv.avatar_url);
-    });
-  }
-
-function downloadImageByURL(url, filePath) {
-  // ...
-  request.get(url)
-  .on('error', function(err) {
-    throw err;
-  })
-  .on('response', function(response) {
-    console.log('Response Status Code: ', response.statusCode);
-  })
-  .pipe(fs.createWriteStream('./' + afilePath));
-}
-
+  request.get(requestURL, function(error, response) {
+    var contributors = (JSON.parse(response.body));
+    //console.log(contributors);
+    for (var i = 0; i < contributors.length; i++) {
+      var loginUser = contributors[i].login;
+      //console.log(loginUser);
+      var filePath = './avatars/' + loginUser + '.jpg';
+      cb(contributors[i].avatar_url, filePath);
+    }
+  });
 
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-  // result.map(function(cv, index){
-  //   console.log(cv);
-  // });
-});
+getRepoContributors("jquery", "jquery", downloadImageByURL);
