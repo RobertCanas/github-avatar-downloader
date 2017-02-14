8de2f98959a71cc917ef1aed82f56c1094c7b1af
Robert Canas <robert_canas@hotmail.com>
2017-02-14 00:56:05 +0000

    Creation of requestURL to file

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,13 @@ var request = require('request');
 
 console.log('Welcome to GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
+var GITHUB_USER = "RobertCanas";
+var GITHUB_TOKEN = "321b033b375060fd93ef57506f6e0b11eb996425";
 
+
+function getRepoContributors(repoOwner, repoName, cb) {
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
