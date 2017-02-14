045765f40bf4ac205e8d1e3e2e45d11181090df6
Robert Canas <robert_canas@hotmail.com>
2017-02-14 03:42:16 +0000

    Change to Step 7

--- download_avatars.js
+++ download_avatars.js
@@ -25,20 +25,18 @@ function getRepoContributors(repoOwner, repoName, cb) {
     });
   }
 
-var request = require('request');
-var fs = require('fs');
-
-var url = "https://avatars2.githubusercontent.com/u/2741?v=3&s=466";
-var filePath = "avatars/kvirani.jpg";
-
-request.get(url + '/' + filePath)
+function downloadImageByURL(url, filePath) {
+  // ...
+  request.get(url)
   .on('error', function(err) {
     throw err;
   })
   .on('response', function(response) {
     console.log('Response Status Code: ', response.statusCode);
   })
-  .pipe(fs.createWriteStream('./avatar/future.jpg'));
+  .pipe(fs.createWriteStream('./' + afilePath));
+}
+
 
 }
 
